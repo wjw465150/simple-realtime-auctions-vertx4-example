b290d31458612ab555e2edb5b15a8431569c1898
wjw465150 <wjw465150@gmail.com>
2023-02-06 09:58:30 +0800

    升级Gradle到7.6版本!

--- run_script/run_as_cluster.bat
+++ run_script/run_as_cluster.bat
@@ -2,7 +2,7 @@ REM 进入当前批处理文件所在的目录
 cd /d %~dp0
 
 REM 指定一个外部文件为自定义配置文件
-java -Dvertx.zookeeper.config=./config/zookeeper.json -jar simple-realtime-auctions-vertx4-example-1.0-fat.jar -cluster
+java -Djava.net.preferIPv4Stack=true -Dvertx.zookeeper.config=./config/zookeeper.json -jar simple-realtime-auctions-vertx4-example-1.0-fat.jar -cluster
 
 REM 从 classpath 中加载一个文件为自定义配置文件
-REM java -Dvertx.zookeeper.config=classpath:config/zookeeper.json -jar simple-realtime-auctions-vertx4-example-1.0-fat.jar -cluster
+REM java -Djava.net.preferIPv4Stack=true -Dvertx.zookeeper.config=classpath:config/zookeeper.json -jar simple-realtime-auctions-vertx4-example-1.0-fat.jar -cluster
